9855d1c907c5f2453d98094b725b80b7b8514516
matebe12 <matebe12@gmail.com>
2021-09-12 05:19:03 +0900

    first

---
+++ ffmmpeg.js
@@ -0,0 +1,19 @@
+const ffmpeg = require('fluent-ffmpeg');
+const ffmpegInstaller = require('@ffmpeg-installer/ffmpeg');
+
+ffmpeg.setFfmpegPath(ffmpegInstaller.path);
+
+module.exports = ffmpeg('videos/edddd.mp4', { timeout: 432000 })
+    .addOptions([
+        '-profile:v baseline',
+        '-level 3.0',
+        '-start_number 0',
+        '-hls_time 10',
+        '-hls_list_size 0',
+        '-f hls',
+    ])
+    .output('videos/edddd.m3u8')
+    .on('end', () => {
+        console.log('end');
+    });
+//.run();
