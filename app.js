5988fa825298f539b4807b4d5a6d21d696388d5d
matebe12 <matebe12@gmail.com>
2021-09-12 12:17:28 +0900

    streaming add

--- app.js
+++ app.js
@@ -17,6 +17,24 @@ app.get('/', (req, res) => {
         }
     }
 });
+app.get('/streaming', (req, res) => {
+    const range = req.headers.range;
+    const videoPath = './videos/edddd.mp4';
+    const videoSize = fs.statSync(videoPath).size;
+    const chunkSize = 128 * 2048;
+    const start = Number(range.replace(/\D/g, ''));
+    const end = Math.min(start + chunkSize, videoSize - 1);
+    const contentLength = end - start + 1;
+    const header = {
+        'Content-Range': `bytes ${start}-${end}/${videoSize}`,
+        'Accept-Ranges': 'bytes',
+        'Content-Length': contentLength,
+        'Content-Type': 'video/mp4',
+    };
+    res.writeHead(206, header);
+    const stream = fs.createReadStream(videoPath, { start, end });
+    stream.pipe(res);
+});
 
 const server = app.listen(3000);
 
